12eb890c18b1d787fd24d813d80c74827349a65b
milkicow <kozlov.aleksandr@phystech.edu>
2025-05-28 14:47:49 +0300

    Add simulation time as cosim script arg

--- cosim.py
+++ cosim.py
@@ -1,21 +1,23 @@
 import subprocess
 import sys
+import argparse
 
 def main():
-    if len(sys.argv) < 2:
-        print("Specify path_to_elf")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Model and simulation comparun')
+    parser.add_argument('--elf', help='Path to ELF file')
+    parser.add_argument('-t', '--time_sim', type=int, default=10000,
+                       help='Model simulation time, default = 10000')
 
-    elffile_path = sys.argv[1]
+    args = parser.parse_args()
 
     try:
-        print(f"Running: ./model -t 10000 -f {elffile_path} > out.log")
+        print(f"Running: ./model -t {args.time_sim} -f {args.elf} > out.log")
         with open("out.log", "w") as out_file:
-            subprocess.run(["./model", "-t", "10000", "-f", elffile_path],
+            subprocess.run(["./model", "-t", str(args.time_sim), "-f", args.elf],
                           stdout=out_file, check=True)
 
-        print(f"Running: ./bin/sim -l 1 -f {elffile_path}")
-        subprocess.run(["./bin/sim", "-l", "1", "-f", elffile_path], check=True)
+        print(f"Running: ./bin/sim -l 1 -f {args.elf}")
+        subprocess.run(["./bin/sim", "-l", "1", "-f", args.elf], check=True)
 
         print("Running: diff full.log out.log")
         subprocess.run(["diff", "full.log", "out.log"], check=True)
