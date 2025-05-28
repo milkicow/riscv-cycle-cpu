3701da3476d9626077010d958bf35e83c42e5e95
milkicow <kozlov.aleksandr@phystech.edu>
2025-05-28 15:34:35 +0300

    Move maindecoder on defined opcodes

--- rtl/maindec.v
+++ rtl/maindec.v
@@ -1,3 +1,5 @@
+`include "instr_opcodes.v"
+
 module maindec(input  logic [6:0] op,
                output logic [1:0] ResultSrc,
                output logic       MemWrite,
@@ -15,15 +17,15 @@ module maindec(input  logic [6:0] op,
     always_comb
         case(op)
             // RegWrite_ImmSrc_ALUSrc_MemWrite_ResultSrc_Branch_ALUOp_Jump_JumpReg_LUIOp
-            7'b0000011: controls = 13'b1_00_1_0_01_0_00_0_0_0; // lw
-            7'b0100011: controls = 13'b0_01_1_1_00_0_00_0_0_0; // sw
-            7'b0110011: controls = 13'b1_xx_0_0_00_0_10_0_0_0; // R-type
-            7'b0110111: controls = 13'b1_xx_1_0_00_0_00_0_0_1; // lui
-            7'b1100011: controls = 13'b0_10_0_0_00_1_01_0_0_0; // beq, bne, blt, bge, bltu, bgeu
-            7'b0010011: controls = 13'b1_00_1_0_00_0_10_0_0_0; // I-type ALU
-            7'b1101111: controls = 13'b1_11_0_0_10_0_00_1_0_0; // jal
-            7'b1100111: controls = 13'b1_11_0_0_10_0_00_1_1_0; // jalr
-            default:    controls = 13'bx_xx_x_x_xx_x_xx_x_x_0;
+            `OPCODE_LOAD:   controls = 13'b1_00_1_0_01_0_00_0_0_0; // lw
+            `OPCODE_S_TYPE: controls = 13'b0_01_1_1_00_0_00_0_0_0; // sw
+            `OPCODE_R_TYPE: controls = 13'b1_xx_0_0_00_0_10_0_0_0; // R-type
+            `OPCODE_LUI:    controls = 13'b1_xx_1_0_00_0_00_0_0_1; // lui
+            `OPCODE_B_TYPE: controls = 13'b0_10_0_0_00_1_01_0_0_0; // beq, bne, blt, bge, bltu, bgeu
+            `OPCODE_I_TYPE: controls = 13'b1_00_1_0_00_0_10_0_0_0; // I-type ALU
+            `OPCODE_JAL:    controls = 13'b1_11_0_0_10_0_00_1_0_0; // jal
+            `OPCODE_JALR:   controls = 13'b1_11_0_0_10_0_00_1_1_0; // jalr
+            default:        controls = 13'bx_xx_x_x_xx_x_xx_x_x_0;
         endcase
 
 endmodule
